f5c9189ea1351c2c89878336df86e7905825bc43
Thái Võ Hoàng <thai.vo@3forcom.com.vn>
2021-09-22 11:20:31 +0700

    install morgan

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
 const port = 5555;
 
+app.use(morgan("combined"));
+
 app.get("/", (req, res) => {
-  res.send("Hello ExpressJs 1");
+  //   res.send("Hello ExpressJs 1");
+  res.send(`<h1> Hello ExpressJs 1 </h1>`);
 });
 
 app.listen(port, () => {
